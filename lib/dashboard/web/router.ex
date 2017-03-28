904da1cc3afad5c0d1f3948e5ea4e018f30d8dfa
Matt McFarland <matmcfarland@gmail.com>
2017-03-28 13:13:54 -0500

    Remove old route for web index

--- lib/dashboard/web/router.ex
+++ lib/dashboard/web/router.ex
@@ -15,8 +15,6 @@ defmodule Dashboard.Web.Router do
 
   scope "/", Dashboard.Web do
     pipe_through :browser # Use the default browser stack
-
-    get "/", PageController, :index
   end
 
   # Other scopes may use custom stacks.
