0ccbc7b024312bf2812dcd3817ffa6b68bbedecc
Matt McFarland <matmcfarland@gmail.com>
2017-03-29 16:33:17 -0500

    Add documentation to backend module, license and update todos in readme

--- lib/dashboard/backend.ex
+++ lib/dashboard/backend.ex
@@ -1,5 +1,9 @@
 defmodule Dashboard.Backend do
-  @callback load() :: [Map.t]
+  @moduledoc """
+  Defines a single remote backend that will be loaded as a worker process.
+  """
+
+  @callback load() :: {:ok, [Map.t]}, | {:error, String.t}
 
   defmacro __using__(opts) do
     quote do
@@ -9,10 +13,22 @@ defmodule Dashboard.Backend do
     end
   end
 
+  @doc """
+  Returns the backend modules that have been configured in the application.
+
+  Returns `[module(), module()]`
+  """
+  @spec configured() :: [module()]
   def configured do
     Application.get_env(:dashboard, :backends, [])
   end
 
+  @doc """
+  Find the appropriate backend module for the supplied `name`
+
+  Returns `module()`
+  """
+  @spec for(String.t) :: module()
   def for(name) do
     Enum.find(configured(), &(&1.name == name))
   end
