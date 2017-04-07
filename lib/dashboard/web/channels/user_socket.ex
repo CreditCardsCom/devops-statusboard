ca3c941f18386386b3b06701379976279b9b9d6f
Matt McFarland <matmcfarland@gmail.com>
2017-04-07 18:12:12 -0500

    Update websocket configuration to disregard origin for now

--- lib/dashboard/web/channels/user_socket.ex
+++ lib/dashboard/web/channels/user_socket.ex
@@ -5,7 +5,9 @@ defmodule Dashboard.Web.UserSocket do
   channel "backend:*", Dashboard.Web.BackendChannel
 
   ## Transports
-  transport :websocket, Phoenix.Transports.WebSocket
+  transport :websocket, Phoenix.Transports.WebSocket,
+    check_origin: false
+
   # transport :longpoll, Phoenix.Transports.LongPoll
 
   # Socket params are passed from the client and can
