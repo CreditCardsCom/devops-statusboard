a5f8469af425f89d7e991ca4641776d28d2781b3
Matt McFarland <783024+vanetix@users.noreply.github.com>
2018-03-05 12:08:12 -0600

    Add pub-sub functionality to cache

--- lib/dashboard.ex
+++ lib/dashboard.ex
@@ -1,3 +1,21 @@
 defmodule Dashboard do
+  alias Dashboard.Cache
 
+  defdelegate subscribe(pid), to: Cache
+  defdelegate unsubscribe(pid), to: Cache
+  defdelegate all(), to: Cache
+
+  @doc """
+  Refresh all checks, just kills the pingdom supervisor and lets it restart.
+  """
+  def refresh_checks() do
+    case Process.whereis(Dashboard.Pingdom.Supervisor) do
+      pid when is_pid(pid) ->
+        Process.exit(pid, :kill)
+
+        :ok
+      _ ->
+        :error
+    end
+  end
 end
