1024a483e7d70bda1fc0a0c9435a7b355fbbd89f
Matt McFarland <783024+vanetix@users.noreply.github.com>
2018-03-06 10:09:03 -0600

    Fix missing cache manifest

--- config/prod.exs
+++ config/prod.exs
@@ -15,8 +15,9 @@ use Mix.Config
 # which you typically run after static files are built.
 config :dashboard, DashboardWeb.Endpoint,
   server: true,
-  on_init: {DashboardWeb.Endpoint, :load_from_system_env, []},
-  url: [host: "example.com", port: 80]
+  load_from_system_env: true,
+  url: [host: "example.com", port: 80],
+  cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
 config :logger, level: :info
